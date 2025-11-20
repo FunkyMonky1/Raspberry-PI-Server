bf555ce4587a041e844b811620ce284887de92ef
FunkyMonky1 <159067017+FunkyMonky1@users.noreply.github.com>
2025-11-20 18:18:54 +0100

    Add files via upload

---
+++ src/test/java/com/websitefunkymonky1/pi/PiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websitefunkymonky1.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
