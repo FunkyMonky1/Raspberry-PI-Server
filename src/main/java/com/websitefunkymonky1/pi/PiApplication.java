361857db4c2693173527f739bcf7d8b9697c6334
FunkyMonky1 <felixsteiner80@gmail.com>
2025-11-20 19:23:59 +0100

    Changes and done with uploading files

--- src/main/java/com/websitefunkymonky1/pi/PiApplication.java
+++ src/main/java/com/websitefunkymonky1/pi/PiApplication.java
@@ -1,13 +1,28 @@
 package com.websitefunkymonky1.pi;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class PiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PiApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
