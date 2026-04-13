20ff0094f7588d28bcb1b8b858ba5a37ac6d6af8
Felix Steiner <felixsteiner80@gmail.com>
2026-04-13 17:53:43 +0200

    added category,uploaded by and ip_address to sql schema because i forgot them

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,27 +2,21 @@ CREATE DATABASE IF NOT EXISTS cloud_storage;
        
 USE cloud_storage;
 
-CREATE TABLE users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(255) NOT NULL UNIQUE
-);
 
 CREATE TABLE file_metadata (
-                               id INT AUTO_INCREMENT PRIMARY KEY,
-                               original_filename VARCHAR(255) NOT NULL,
-                               stored_filename VARCHAR(255) NOT NULL,
-                               path VARCHAR(500) NOT NULL,
-                               size BIGINT NOT NULL,
-                               content_type VARCHAR(100),
-                               upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                               user_id INT,
-                               CONSTRAINT fk_file_user FOREIGN KEY (user_id) REFERENCES users(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    original_filename VARCHAR(255) NOT NULL,
+    stored_filename VARCHAR(255) NOT NULL,
+    path VARCHAR(500) NOT NULL,
+    size BIGINT NOT NULL,
+    content_type VARCHAR(100),
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    uploaded_by VARCHAR(255),
+    ip_address VARCHAR(45),
+    category VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE file_metadata 
-ADD COLUMN ip_address varchar(45);
-/* 45 is more than enough for this  IPv4 and IPv6
-*/
+
 
 
 
