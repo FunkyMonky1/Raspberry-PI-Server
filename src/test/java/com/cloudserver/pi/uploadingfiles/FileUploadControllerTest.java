d5afa1fa563e29ade107d48a9a43cd82935dc19f
Felix Steiner <felixsteiner80@gmail.com>
2026-04-18 23:49:31 +0200

    wrote first test for FileUploadController

--- src/test/java/com/cloudserver/pi/uploadingfiles/FileUploadControllerTest.java
+++ src/test/java/com/cloudserver/pi/uploadingfiles/FileUploadControllerTest.java
@@ -1,23 +1,51 @@
 package com.cloudserver.pi.uploadingfiles;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest 
-
+@AutoConfigureMockMvc
 class FileUploadControllerTest {
     
+    @Autowired
+    private MockMvc mockMvc;
+    
     @Test
-    void uploadAllowedFile_shouldSucceed(){
+    @WithMockUser(username= "felix", roles ="USER")
+    void uploadAllowedFile_shouldSucceed() throws Exception{
+        //Arrange
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "test.pdf",
+                "application/pdf",
+                "test content".getBytes()
+        );
+        //Act
+        var result = mockMvc.perform(multipart("/")
+                .file(file)
+                .param("category", "MATH"));
         
-        String filename = "testing.pdf";
         
-        String extension =  filename.substring(filename.lastIndexOf(".")+1);
+        //Assert
+        result.andExpect(status().is3xxRedirection())
+                .andExpect(flash().attribute("message",
+                        "You successfully uploaded test.pdf!"));
         
         
-        assertEquals("pdf", extension);
+        
+        
+        
+        
+        
+        
         
     }
 }
